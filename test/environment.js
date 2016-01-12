605f97ce0e094c52ef99138a3cd27fa0bda01933
ilya-b <ilya-b@yandex-team.ru>
2016-01-12 14:54:53 +0300

    Simplify test/environment

--- test/environment.js
+++ test/environment.js
@@ -6,17 +6,6 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import sinon from 'sinon';
 
-export function set() {
-    chai.use(chaiAsPromised);
-    sinon.assert.expose(chai.assert, {prefix: ''});
-    setGlobal('sinon', sinon);
-    setGlobal('assert', chai.assert);
-}
-
-export function reset() {
-    resetGlobals();
-}
-
 var oldValuesMap = {};
 
 function setGlobal(name, value) {
@@ -24,16 +13,7 @@ function setGlobal(name, value) {
     global[name] = value;
 }
 
-function resetGlobals() {
-    Object.keys(oldValuesMap).forEach(function (name) {
-        var oldValue = oldValuesMap[name];
-        if (oldValue !== undefined) {
-            global[name] = oldValue;
-        } else {
-            delete global[name];
-        }
-    });
-}
-
-// Automatically set test environment
-set();
+chai.use(chaiAsPromised);
+sinon.assert.expose(chai.assert, {prefix: ''});
+setGlobal('sinon', sinon);
+setGlobal('assert', chai.assert);
